3f651cb340fd510247602c8d0e782c6206fbddd9
mmlado-cpp <98682021+mmlado-cpp@users.noreply.github.com>
2022-04-26 13:29:23 -0700

    added some of the graph operations

--- DirectedGraph.java
+++ DirectedGraph.java
@@ -20,5 +20,82 @@ public class DirectedGraph<T> implements GraphInterface<T>
 
 /* Implementations of the graph operations go here. 
    . . . */
+   public QueueInterface<T> getBreadthFirstTraversal(T origin)
+    {
+    resetVertices();
+    QueueInterface<T> traversalOrder = new LinkedQueue<>();
+    QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<>();
+    
+    VertexInterface<T> originVertex = vertices.getValue(origin);
+    originVertex.visit();
+    traversalOrder.enqueue(origin);    // Enqueue vertex label
+    vertexQueue.enqueue(originVertex); // Enqueue vertex
+
+    while (!vertexQueue.isEmpty())
+    {
+        VertexInterface<T> frontVertex = vertexQueue.dequeue();
+        Iterator<VertexInterface<T>> neighbors = frontVertex.getNeighborIterator();
+
+        while (neighbors.hasNext())
+        {
+            VertexInterface<T> nextNeighbor = neighbors.next();
+            if (!nextNeighbor.isVisited())
+            {
+                nextNeighbor.visit();
+                traversalOrder.enqueue(nextNeighbor.getLabel());
+                vertexQueue.enqueue(nextNeighbor);
+            } // end if
+        } // end while
+    } // end while
+
+    return traversalOrder;
+    } // end getBreadthFirstTraversal
+
+    public int getShortestPath(T begin, T end, StackInterface<T> path)
+    {
+        resetVertices();
+        boolean done = false;
+        QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<>();
+        VertexInterface<T> originVertex = vertices.getValue(begin);
+        VertexInterface<T> endVertex = vertices.getValue(end);
+        originVertex.visit();
+
+        // Assertion: resetVertices() has executed setCost(0)
+        // and setPredecessor(null) for originVertex
+
+        vertexQueue.enqueue(originVertex);
+        while (!done && !vertexQueue.isEmpty())
+        {
+            VertexInterface<T> frontVertex = vertexQueue.dequeue();
+            Iterator<VertexInterface<T>> neighbors = frontVertex.getNeighborIterator();
+            while (!done && neighbors.hasNext())
+            {
+                VertexInterface<T> nextNeighbor = neighbors.next();
+                if (!nextNeighbor.isVisited())
+                {
+                    nextNeighbor.visit();
+                    nextNeighbor.setCost(1 + frontVertex.getCost());
+                    nextNeighbor.setPredecessor(frontVertex);
+                    vertexQueue.enqueue(nextNeighbor);
+                } // end if
+
+                if (nextNeighbor.equals(endVertex))
+                    done = true;
+            } // end while
+        } // end while
+
+        // Traversal ends; construct shortest path
+        int pathLength = (int)endVertex.getCost();
+        path.push(endVertex.getLabel());
+
+        VertexInterface<T> vertex = endVertex;
+        while (vertex.hasPredecessor())
+        {
+            vertex = vertex.getPredecessor();
+            path.push(vertex.getLabel());
+        } // end while
+
+        return pathLength;
+    } // end getShortestPath
   
 } // end DirectedGraph
